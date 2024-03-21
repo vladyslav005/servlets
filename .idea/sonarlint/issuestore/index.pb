0e0f105042ad09b6ae5d606b04b009ea281983f6
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2024-03-21 21:46:20 +0100

    add template engine

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,7 +5,11 @@ Y
 )servletsbasics/src/main/webapp/index.html,0/8/082df197bf450578caa5aef806ef3f439e7e5ba4
 i
 9servletsbasics/src/main/java/org/example/TimeServlet.java,6/5/656762254a96f136a224f5258b56ba1e83bcf876
-b
-2servletsbasics/src/main/java/org/example/Test.java,e/4/e4791db459d1f1a6e7acee6b87cb9d79e5746d37
 t
 Dservletsbasics/src/main/java/org/example/TimezoneValidateFilter.java,9/9/997433ea7d9fe5838a134de4f3965a5d14066654
+b
+2servletsbasics/src/main/java/org/example/Main.java,4/4/444bcf59d61d8699284727abc3d0b3474920e9ad
+b
+2servletsbasics/src/main/java/org/example/Ping.java,3/b/3bca45bedc1cbc24ad204295e0b6bc9b4d73bea3
+e
+5servletsbasics/src/main/webapp/WEB-INF/temp/time.html,1/d/1dc1c4928caba550a40e706b4f612f52a85ace3e
--- servletsbasics/src/main/java/org/example/TimeServlet.java
+++ servletsbasics/src/main/java/org/example/TimeServlet.java
@@ -1,6 +1,14 @@
 package org.example;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JavaxServletWebApplication;
+
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,32 +17,63 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 
 @WebServlet({"/time"})
 public class TimeServlet extends HttpServlet {
 
+    private final TemplateEngine templateEngine = new TemplateEngine();
+    private final Map<String, String> coockiesMap = new HashMap<>();
+
+
+    @Override
+    public void init() {
+        JavaxServletWebApplication javaxServletWebApplication =
+                JavaxServletWebApplication.buildApplication(this.getServletContext());
+        WebApplicationTemplateResolver resolver = new WebApplicationTemplateResolver(javaxServletWebApplication);
+
+        resolver.setPrefix("/WEB-INF/temp/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+
+        templateEngine.addTemplateResolver(resolver);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
         resp.setContentType("text/html");
-
-        PrintWriter out = resp.getWriter();
+        String formattedDateTime;
 
         String urlParam = req.getParameter("time");
 
-        if (urlParam == null) urlParam = "UTC+0";
 
-        urlParam = urlParam.replace(" ", "+");
+        if (req.getCookies() != null)
+            Arrays.stream(req.getCookies()).forEach((c) ->
+                coockiesMap.put(c.getName(), c.getValue()));
+
+        if (urlParam == null) {
+            urlParam = coockiesMap.getOrDefault("lastTimezone", "UTC+0");
+            formattedDateTime = getFormattedTime(urlParam);
+        } else {
+            urlParam = urlParam.replace(" ", "+");
+            formattedDateTime = getFormattedTime(urlParam);
+            Cookie cookie = new Cookie("lastTimezone", urlParam);
+            cookie.setMaxAge(10 * 60);
+            resp.addCookie(cookie);
+        }
+
+        Context context = new Context(req.getLocale(), Map.of("timeText", formattedDateTime));
+        templateEngine.process("time", context, resp.getWriter());
+    }
+
+    private String getFormattedTime(String urlParam) {
         LocalDateTime localNow = LocalDateTime.now(TimeZone.getTimeZone(ZoneId.of(urlParam)).toZoneId());
-        String formattedDateTime = localNow.format(DateTimeFormatter.ofPattern("YYYY-MM-DD HH:mm:ss"));
-
-        out.println("<html><body>");
-        out.println("<h1> ${time} ${offset}</h1>"
-                .replace("${time}", urlParam)
-                .replace("${offset}", formattedDateTime));
-        out.println("</html></body>");
-
-        out.close();
+        return localNow.format(DateTimeFormatter.ofPattern("YYYY-MM-DD HH:mm:ss")) + " " + urlParam;
     }
 }
